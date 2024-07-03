18d3c57d964678a2573684a171b5f251368b8f45
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-07-03 21:18:43 +0300

    Add navigation and About Component

---
+++ portofolio-app/src/Components/About.js
@@ -0,0 +1,21 @@
+import "../Styles/About.css";
+import photo from "../image.png";
+export default function About() {
+  return (
+    <div className="Container">
+      <div>
+        <img src={photo} width={100}></img>
+      </div>
+      <div>
+        <h2>Hello, its Me Mahmoud Reda And I'm a Full Stack Developer</h2>
+        <p>
+          {" "}
+          I'm a student eager to become a software engineer through learning web
+          development. Seeking training opportunities to refine my skills in
+          this field. I'm passionate about learning and committed to achieving
+          proficiency in web development
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ portofolio-app/src/Components/NavigationBar.js
@@ -0,0 +1,26 @@
+import "../Styles/Navigation.css";
+export default function Nav() {
+  return (
+    <div class="container">
+      <div class="navbar">
+        <h2>Sharaf</h2>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Portfolio</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
