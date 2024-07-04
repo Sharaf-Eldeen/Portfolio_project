d80af2b32509d9e9d92d03ad9d17fe05bf4dc1a9
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-07-04 04:08:01 +0300

    Edit About Component to include two Buttons

--- portofolio-app/src/Components/About.js
+++ portofolio-app/src/Components/About.js
@@ -1,21 +1,25 @@
 import "../Styles/About.css";
 import photo from "../image.png";
+import ColorButtons from "./Button.js";
 export default function About() {
   return (
-    <div className="Container">
-      <div>
-        <img src={photo} width={100}></img>
+    <>
+      <div className="Container">
+        <div>
+          <img src={photo} width={100}></img>
+        </div>
+        <div>
+          <h2>Hello, its Me Mahmoud Reda And I'm a Full Stack Developer</h2>
+          <p>
+            {" "}
+            I'm a student eager to become a software engineer through learning
+            web development. Seeking training opportunities to refine my skills
+            in this field. I'm passionate about learning and committed to
+            achieving proficiency in web development
+          </p>
+        </div>
+        <ColorButtons />
       </div>
-      <div>
-        <h2>Hello, its Me Mahmoud Reda And I'm a Full Stack Developer</h2>
-        <p>
-          {" "}
-          I'm a student eager to become a software engineer through learning web
-          development. Seeking training opportunities to refine my skills in
-          this field. I'm passionate about learning and committed to achieving
-          proficiency in web development
-        </p>
-      </div>
-    </div>
+    </>
   );
 }
