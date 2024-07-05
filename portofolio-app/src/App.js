844c4bf9ec465719cd0949d57599e35449970382
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-07-05 09:48:01 +0300

    Update App Component  To Render Footer Component

--- portofolio-app/src/App.js
+++ portofolio-app/src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import ResponsiveAppBar from "./Components/NavigationBar.js";
 import About from "./Components/About.js";
+import Footer from "./Components/Footer.js";
 
 function App() {
   return (
     <div>
       <ResponsiveAppBar />
       <About />
+      <Footer />
     </div>
   );
 }
