2e74bd80b40c4594835978ac2e53c74e8cc170ec
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-09-04 12:19:06 +0300

    Add all components to App component

--- portofolio-app/src/App.js
+++ portofolio-app/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import ResponsiveAppBar from "./Components/NavigationBar.js";
-import About from "./Components/About.js";
+import ToggleComponents from "./Components/ToggleComponent.js";
 import Footer from "./Components/Footer.js";
+import Projects from "./Components/Projects.js";
 
 function App() {
   return (
     <div>
       <ResponsiveAppBar />
-      <About />
+      <ToggleComponents />
+      <Projects />
       <Footer />
     </div>
   );
