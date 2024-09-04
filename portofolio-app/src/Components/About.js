c01cbe0ede8ece7d818473892def948bc757f4aa
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-09-04 12:13:53 +0300

    Edit The Content in About Component

--- portofolio-app/src/Components/About.js
+++ portofolio-app/src/Components/About.js
@@ -1,22 +1,17 @@
 import "../Styles/About.css";
-import photo from "../image.png";
 import ColorButtons from "./Button.js";
-export default function About() {
+export default function Cover() {
   return (
     <>
       <div className="Container">
         <div>
-          <img src={photo} width={100}></img>
-        </div>
-        <div>
-          <h2>Hello, its Me Mahmoud Reda And I'm a Full Stack Developer</h2>
-          <p>
-            {" "}
-            I'm a student eager to become a software engineer through learning
-            web development. Seeking training opportunities to refine my skills
-            in this field. I'm passionate about learning and committed to
-            achieving proficiency in web development
-          </p>
+          <h1>About</h1>
+          <h4>
+            Seeking an internship as a Software Engineer with a focus on MERN
+            Stack development, where I can apply my skills in building dynamic,
+            full-stack web applications and further enhance my expertise in
+            Node.js, Express, React, and MongoDB.{" "}
+          </h4>
         </div>
         <ColorButtons />
       </div>
