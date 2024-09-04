963271ba4efc9afac0fdf2e6308853c0d87099c4
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-09-04 12:12:25 +0300

    Add functionalities to buttons to download cv and send emails

--- portofolio-app/src/Components/Button.js
+++ portofolio-app/src/Components/Button.js
@@ -4,10 +4,19 @@ import Button from "@mui/material/Button";
 export default function ColorButtons() {
   return (
     <Stack direction="row" spacing={30} sx={{ marginTop: "8px" }}>
-      <Button variant="contained" color="primary">
+      <Button
+        variant="contained"
+        color="primary"
+        href="/files/MahmoudRedaResume.pdf"
+        download="MahmoudRedaResume.pdf"
+      >
         Download CV
       </Button>
-      <Button variant="contained" color="primary">
+      <Button
+        variant="contained"
+        color="primary"
+        href="mailto:mahmoudsharaf796@gmail.com"
+      >
         Contact me
       </Button>
     </Stack>
